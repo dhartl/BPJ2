667a398fb9ffca2542b2ff7acabecaa3d4466211
MichaelGmoser <michael.gmoser@edu.campus02.at>
2016-06-05 18:38:39 +0200

    client.service: CustomerService Klasse angelegt

---
+++ at.c02.bpj.client/src/main/java/at/c02/bpj/client/service/CustomerService.java
@@ -0,0 +1,25 @@
+package at.c02.bpj.client.service;
+
+import java.util.List;
+
+import at.c02.bpj.client.api.CustomerApi;
+import at.c02.bpj.client.api.model.Customer;
+
+
+public class CustomerService {
+	
+	private CustomerApi customerApi;
+
+	//Konstruktor  API Klasse 
+	public CustomerService(CustomerApi customerApi) {
+		super();
+		this.customerApi = customerApi;
+	}
+
+	//ServiceKlasse executeCall aufrufen --> Response anfordern
+	public List<Customer> getCustomer() {
+	
+		return Services.executeCall(customerApi.getCustomer());
+	}
+
+}
