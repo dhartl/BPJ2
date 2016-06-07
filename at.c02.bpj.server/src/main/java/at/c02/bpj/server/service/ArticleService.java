a1d754094f8ed250c9c5442971826004a64d8df7
Philipp <Philipp@Elitebook>
2016-06-08 00:04:05 +0200

    ) implemented OfferPositionRepository & OfferPositionService + typo

--- at.c02.bpj.server/src/main/java/at/c02/bpj/server/service/ArticleService.java
+++ at.c02.bpj.server/src/main/java/at/c02/bpj/server/service/ArticleService.java
@@ -14,41 +14,41 @@ import at.c02.bpj.server.repository.ArticleRepository;
 @Service
 public class ArticleService {
 
-	private ArticleRepository articleRepository;
+    private ArticleRepository articleRepository;
 
-	// Spring managt diese Klasse. @Autowired-Felder werden automatisch befüllt
-	@Autowired
-	public void setArticleRepository(ArticleRepository articleRepository) {
-		this.articleRepository = articleRepository;
-	}
+    // Spring managed diese Klasse. @Autowired-Felder werden automatisch befüllt
+    @Autowired
+    public void setArticleRepository(ArticleRepository articleRepository) {
+	this.articleRepository = articleRepository;
+    }
 
-	/**
-	 * findet alle Artikel
-	 * 
-	 * @return Liste aller Artikel
-	 */
-	public List<Article> getAllArticles() {
-		return articleRepository.findAll();
-	}
+    /**
+     * findet alle Artikel
+     * 
+     * @return Liste aller Artikel
+     */
+    public List<Article> getAllArticles() {
+	return articleRepository.findAll();
+    }
 
-	/**
-	 * löscht den Artikel mit der Id articleId
-	 * 
-	 * @param articleId
-	 */
-	public void deleteArticle(long articleId) {
-		articleRepository.delete(articleId);
-	}
+    /**
+     * löscht den Artikel mit der Id articleId
+     * 
+     * @param articleId
+     */
+    public void deleteArticle(long articleId) {
+	articleRepository.delete(articleId);
+    }
 
-	/**
-	 * erstellt oder aktualisiert den Artikel. Ob ein Artikel bereits existiert
-	 * wird an der ArtikelId bestimmt
-	 * 
-	 * @param article
-	 * @return der gespeicherte Artikel
-	 */
-	public Article createOrUpdateArticle(Article article) {
-		return articleRepository.save(article);
-	}
+    /**
+     * erstellt oder aktualisiert den Artikel. Ob ein Artikel bereits existiert
+     * wird an der ArtikelId bestimmt
+     * 
+     * @param article
+     * @return der gespeicherte Artikel
+     */
+    public Article createOrUpdateArticle(Article article) {
+	return articleRepository.save(article);
+    }
 
 }
