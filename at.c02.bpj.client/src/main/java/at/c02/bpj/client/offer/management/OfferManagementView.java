70a122d83da354b70703a7ab191c1867445efc03
MichaelGmoser <michael.gmoser@edu.campus02.at>
2016-06-12 11:34:30 +0200

    UC006: FXML enternen AnchorPane, View und View Model: Laden von Employee und Kunden in TableView; Implementierung Suchfunktionen

--- at.c02.bpj.client/src/main/java/at/c02/bpj/client/offer/management/OfferManagementView.java
+++ at.c02.bpj.client/src/main/java/at/c02/bpj/client/offer/management/OfferManagementView.java
@@ -2,17 +2,24 @@ package at.c02.bpj.client.offer.management;
 
 
 import java.net.URL;
-import java.sql.Date;
 import java.text.DecimalFormat;
+import java.text.ParsePosition;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.ResourceBundle;
+import java.util.function.Predicate;
 
 import at.c02.bpj.client.api.model.Customer;
 import at.c02.bpj.client.api.model.Employee;
 import at.c02.bpj.client.api.model.Offer;
 import at.c02.bpj.client.api.model.OfferPosition;
+import at.c02.bpj.client.converter.StringToNumberConverter;
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
@@ -25,10 +32,13 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
+import javafx.util.Callback;
 import javafx.util.StringConverter;
 
 /**
@@ -67,44 +77,55 @@ public class OfferManagementView implements FxmlView<OfferManagementViewModel>, 
 	private TableColumn<Offer, Date> createdDateColumn;
 	@FXML
 	private TableColumn<Offer, Date> completedDateColumn;
-	
-//--> Typ STring Foreign Keys ???? 
 	@FXML
-	private TableColumn<Offer, Employee> employeeColumn;
+	private TableColumn<Offer, String> employeeColumn;
 	@FXML
-	private TableColumn<Offer, Customer> customerColumn;
+	private TableColumn<Offer, String> customerColumn;
 	@FXML
 	private TableColumn<Offer, String> statusColumn;
 	@FXML
-	private TableColumn<OfferPosition, DecimalFormat> priceColumn;
-//-- > wie/wo berechnen Menge * Preis ? 
-	
+	private TableColumn<Offer, Double> priceColumn; 
 	
 	@FXML
 	private MenuItem closeMenuItem;
+
+	@FXML
+	private FilteredList<Offer> filteredData;
 	
 	
-	// Initialize --> Tabellen Werte wo bereits bei Aufruf/Beginn UC Werte enthalten sein sollen
 	@Override
 	public void initialize(URL location, ResourceBundle resources) { 
 		
+		//OfferTable binden Syntax: Bindings (List<> , ObservableList<>)
+		Bindings.bindContent(offerTable.itemsProperty().get(), model.offerListProperty());	
 		
-		Bindings.bindContent(offerTable.itemsProperty().get(), model.offerListProperty());
-		//offersPositionsList? binding??? 
-		
-//---> Daniel Filtered Liste an OfferTable binden
-		//Bindings.bindContent(model.getFilteredData(), offerTable.itemsProperty().get());
-		
-		
-		//Alle involvierten Spalten binden -- Syntax: Bindings (List<> , ObservableList<>)
+		//Alle involvierten Spalten der OfferTable binden
 		offerNumberColumn.setCellValueFactory(new PropertyValueFactory<>("offerId"));
 		createdDateColumn.setCellValueFactory(new PropertyValueFactory<>("createdDt"));
 		completedDateColumn.setCellValueFactory(new PropertyValueFactory<>("completedDt"));
 		statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
+		
+//Daniel -->Binding für PreisSpalte?	
 		priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
-	
-		// für ComboBox Employee (Binding zwischen ComboBox Feld und aus Modell
-		// mit der Employee Property
+		//Bindings.bindContent(priceColumn.getText(), model.offerPositionListProperty());
+		
+		//laden von ForeignKey Felder der OfferTable (Employee und Customer) 
+		customerColumn.setCellValueFactory(new Callback<CellDataFeatures<Offer, String>,ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(CellDataFeatures<Offer, String> param) {
+                return new SimpleStringProperty(param.getValue().getCustomer().getCompanyName());
+            }
+        });
+		
+		employeeColumn.setCellValueFactory(new Callback<CellDataFeatures<Offer, String>,ObservableValue<String>>(){
+            @Override
+            public ObservableValue<String> call(CellDataFeatures<Offer, String> param) {
+                return new SimpleStringProperty(param.getValue().getEmployee().getLastname());
+            }
+        });
+		
+
+		// für ComboBox Employee (Binding zwischen ComboBox Feld und EmployeeProperty
 		Bindings.bindContent(employeeField.itemsProperty().get(), model.employeeListProperty());
 		employeeField.valueProperty().bindBidirectional(model.employeeProperty());	
 		
@@ -136,65 +157,203 @@ public class OfferManagementView implements FxmlView<OfferManagementViewModel>, 
 			}
 		});
 	
-	    }
-		 
-	// Getter für SuchfunktionenFelder UI
-	public TextField getOfferField() {
-			return offerField;
-		}
-	public DatePicker getDateStartField() {
-			return dateStartField;
-	}
-	public DatePicker getDateEndField() {
-			return dateEndField;
-	}
-	public ComboBox<Customer> getCustomerField() {
-			return customerField;
-	}
-	public ComboBox<Employee> getEmployeeField() {
-			return employeeField;
+		
+		// OfferTableObservable liste in FilteredList wandeln
+        filteredData = new FilteredList<>(model.offerListProperty());
+    
+        // Alle Daten anzeigen zu Beginn; Predicate anzeigen = true 
+        Predicate<Offer> predicate = new Predicate<Offer>() {
+			@Override
+			public boolean test(Offer t) {
+			    return true;
+			}
+        };
+        //filteredData.setPredicate(predicate);
+        filteredData.setPredicate(predicate);
+	
+	    // OfferTable mit gefilterterList aus Suche befüllen
+        //offerTable.setItems(filteredData);
+        offerTable.setItems(filteredData);
 	}
 
-		
-		// wenn Suchen klickt, soll 1. ANR: logik aufrufen, dann Zeitraum etc...(immer nur mit filteredList danach weiter) 
-		//FilterTable aufrufen
+
+	
+	//Getter/Setter SearchFields 
+	public final TextField getOfferField() {
+		return offerField;
+	}
+	public final void setOfferField(TextField offerField) {
+		this.offerField = offerField;
+	}
+	public final DatePicker getDateStartField() {
+		return dateStartField;
+	}
+	public final void setDateStartField(DatePicker dateStartField) {
+		this.dateStartField = dateStartField;
+	}
+	public final DatePicker getDateEndField() {
+		return dateEndField;
+	}
+	public final void setDateEndField(DatePicker dateEndField) {
+		this.dateEndField = dateEndField;
+	}
+	public final ComboBox<Customer> getCustomerField() {
+		return customerField;
+	}
+	public final void setCustomerField(ComboBox<Customer> customerField) {
+		this.customerField = customerField;
+	}
+	public final ComboBox<Employee> getEmployeeField() {
+		return employeeField;
+	}
+	public final void setEmployeeField(ComboBox<Employee> employeeField) {
+		this.employeeField = employeeField;
+	}
+
+
+	@FXML
 	public void onSearchButtonClick() {
 		
 		// wenn kein Suchfeld befüllt wurde (=alle leer)
-			if (offerField.getText().trim().equals("") && dateStartField.getValue() == null && 
-					dateEndField.getValue() == null && customerField.getValue() == null &&
-						employeeField.getValue() == null) {
+		if (offerField.getText().trim().equals("") && dateStartField.getValue() == null && 
+				dateEndField.getValue() == null && customerField.getValue() == null &&
+				employeeField.getValue() == null) {
 		
 				 Alert noInputAlert= new Alert(AlertType.WARNING);
 			        noInputAlert.setHeaderText("Keine Eingabe vorhanden");
 			        noInputAlert.setContentText("Bitte mindestens ein Suchkriterium eingeben");
 			        noInputAlert.showAndWait();
-			   
-			}
-	
-			//wenn mindestens 1 Feld Input hat 
-			else {
-				searchButton.setOnAction(model.FindByEmployee());
-	/*			searchButton.setOnAction(model.FindByDate());
-			
-				searchButton.setOnAction(model.FindByOfferNumber());
-				searchButton.setOnAction(model.FindByCustomer());
-				
-				
-				SortedList<Offer> sortedData = new SortedList<>(model.getFilteredData());
-			     
-			        // SortedList binden, comparator zum TableView comparator.
-			        sortedData.comparatorProperty().bind(offerTable.comparatorProperty());
-			       
-			// Sortierte und gefilterte Daten in Tabelle einfügen
-			        offerTable.setItems(sortedData); 
-	*/
-			}
-			
-		// Filterliste löschen
-			model.getFilteredData().clear();
-
 		}
+			
+//--->Daniel -- wie /wo Predicate definieren, dass nicht alle Fälle aufrufen muss?
+		
+			/// 4x if wenn suchfeld jeweils alleine nicht leer
+		if (offerField.getText() != null) {
+				//FindByOfferNumber();
+				FindByOfferNumber();
+		}
+		if (dateStartField.getValue() != null && dateEndField != null) {
+				FindByDate();
+		}
+		if (customerField.getValue() != null) {
+				FindByCustomer();
+		}
+		if (employeeField.getValue() != null) {
+				FindByEmployee();
+		}					
+	}
+			
+	
+public void FindByOfferNumber() {
+		
+		String newValue = offerField.getText();
+	
+		Predicate<Offer> predicate = new Predicate<Offer>() {
+
+			@Override
+			public boolean test(Offer offerNumber) {
+				if (newValue == null || newValue.isEmpty()) {
+					return true;
+				}
+				String lowerCaseFilter = newValue.toLowerCase();
+				
+				if (offerNumber.getOfferId().toString().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+					return true; // Filter matched OfferNumber
+				} 
+				return false; // Kein Match
+			}
+			
+		};
+		filteredData.setPredicate(predicate);	
+	}		
+	
+
+	public void FindByDate() {
+		
+		// werte von ComboBox
+		LocalDate startDate = dateStartField.getValue();
+		LocalDate endDate = dateEndField.getValue();
+		
+		Date Sdate = Date.from(startDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		Date Edate = Date.from(endDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		
+		Predicate<Offer> predicate = new Predicate<Offer>() {
+
+			@Override
+			public boolean test(Offer offerDate) {
+				//wenn leer, alle anzeigen
+				if (Sdate == null || Edate == null) {
+					return true;
+				}
+				
+				int compareDateFrom = offerDate.getCreatedDt().compareTo(Sdate);
+				if (compareDateFrom >=0) {
+					 // Filter matches OfferNumber
+					int compareDateTo = offerDate.getCompletedDt().compareTo(Edate);
+					
+					if (compareDateTo <=0) {
+						return true;
+					}
+				} 
+				return false; // Kein Match
+			}
+			
+		};
+		filteredData.setPredicate(predicate);
+	}
+	
+	
+	
+	private void FindByCustomer() {
+
+		String cust = (String) customerField.getValue().getCompanyName();
+		
+		Predicate<Offer> predicate = new Predicate<Offer>() {
+		
+			@Override
+			public boolean test(Offer customer) {
+				if (cust == null || cust.isEmpty()) {
+					return true;
+				}
+				String lowerCaseFilter = cust.toLowerCase();
+				
+				if (customer.getCustomer().getCompanyName().toString().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+					
+					return true; // Filter matches OfferNumber
+				} 
+				return false; // Kein Match
+			}
+			
+		};
+		filteredData.setPredicate(predicate);	
+	}
+	
+	
+	public void FindByEmployee() {
+		
+		String empl = (String) employeeField.getValue().getLastname();
+		
+		Predicate<Offer> predicate = new Predicate<Offer>() {
+		
+			@Override
+			public boolean test(Offer employee) {
+				if (empl == null || empl.isEmpty()) {
+					return true;
+				}
+				String lowerCaseFilter = empl.toLowerCase();
+				
+				if (employee.getEmployee().getLastname().toString().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+					
+					return true; // Filter matches OfferNumber
+				} 
+				return false; // Kein Match
+			}
+			
+		};
+		filteredData.setPredicate(predicate);	
+	}
+	
+	
 	
 	// Beende 
 	public void closeOfferManagement() {
@@ -202,4 +361,4 @@ public class OfferManagementView implements FxmlView<OfferManagementViewModel>, 
 	}
 
 
-	}
+}
