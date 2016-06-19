1de25e927d3b1fd4d0931fce5b17649df442881a
teschl <teschl@teschl-n56.local>
2016-06-19 16:53:26 +0200

    Anpassung der Maske, Implementierung der Suche für Artikelmanagement

--- at.c02.bpj.client/src/main/java/at/c02/bpj/client/article/ArticleView.java
+++ at.c02.bpj.client/src/main/java/at/c02/bpj/client/article/ArticleView.java
@@ -6,18 +6,29 @@ import java.util.ResourceBundle;
 import org.controlsfx.control.table.TableFilter;
 
 import at.c02.bpj.client.api.model.Article;
-import at.c02.bpj.client.api.model.Offer;
+import at.c02.bpj.client.api.model.Category;
+
+
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
+import javafx.util.Callback;
+import javafx.util.StringConverter;
 
 /**
  * Controller für ArticleView.fxml.
@@ -36,16 +47,39 @@ public class ArticleView implements FxmlView<ArticleViewModel>, Initializable {
 	private TableColumn<Article, String> nameColumn;
 	@FXML
 	private TableColumn<Article, Double> priceColumn;
+	@FXML
+	private TableColumn<Article, String> categoryColumn;
+	
+	@FXML
+	private GridPane searchGridPane;
+	@FXML
+	private TextField idField;
+	@FXML
+	private TextField nameField;
+	@FXML
+	private ComboBox<Category> categoryField;
+	@FXML
+	private Button searchButton;
+	
 	
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// Die Artikel-Tabelle zeigt genau das an, was in der ArticlesProperty
 		// des Models ist
+		
 		Bindings.bindContent(tblArticles.itemsProperty().get(), model.articlesProperty());
 		idColumn.setCellValueFactory(new PropertyValueFactory<>("articleId"));
 		nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
 		priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+
+		categoryColumn.setCellValueFactory(new Callback<CellDataFeatures<Article, String>, ObservableValue<String>>() {
+			@Override
+			public ObservableValue<String> call(CellDataFeatures<Article, String> param) {
+				return new SimpleStringProperty(param.getValue().getCategory().getName());
+			}
+		});
 		
 		// Einfügen des Kontext-Menüs für jede Zeile
 		tblArticles.setRowFactory(table -> {
@@ -53,6 +87,31 @@ public class ArticleView implements FxmlView<ArticleViewModel>, Initializable {
 			row.setContextMenu(createContextMenu(row));
 			return row;
 		});
+		
+		// für ComboBox Category (Binding zwischen ComboBox Feld und
+		// CategoryProperty
+		Bindings.bindContent(categoryField.itemsProperty().get(), model.categoryProperty());
+		categoryField.valueProperty().bindBidirectional(model.searchCategoryProperty());
+
+		categoryField.setConverter(new StringConverter<Category>() {
+
+			@Override
+			public String toString(Category category) {
+				return category.getName();
+			}
+
+			@Override
+			public Category fromString(String string) {
+				return null;
+			}
+		});
+		
+
+		idField.textProperty().bindBidirectional(model.searchArticleIdProperty());
+		nameField.textProperty().bindBidirectional(model.searchArticleNameProperty());
+		
+		
+		
 	}
 
 	private ContextMenu createContextMenu(TableRow<Article> row) {
@@ -86,6 +145,11 @@ public class ArticleView implements FxmlView<ArticleViewModel>, Initializable {
 
 	public void onDeleteArticleClick(Article article) {
 		model.deleteArticle(article);
+	}
+	
+	@FXML
+	public void onSearchButtonClick() {
+		model.onSearchButtonClick();
 	}
 	
 	//OfferManagment UC006 öffnen im Fenster
--- at.c02.bpj.client/src/main/java/at/c02/bpj/client/article/ArticleViewModel.java
+++ at.c02.bpj.client/src/main/java/at/c02/bpj/client/article/ArticleViewModel.java
@@ -1,15 +1,29 @@
 package at.c02.bpj.client.article;
 
+
 import java.util.List;
 import java.util.Optional;
 
+import com.google.common.base.Objects;
+import com.google.common.base.Strings;
+import com.google.common.primitives.Longs;
+
 import at.c02.bpj.client.Async;
 import at.c02.bpj.client.api.model.Article;
+import at.c02.bpj.client.api.model.Category;
+
 import at.c02.bpj.client.offer.management.OfferManagementDialog;
 import at.c02.bpj.client.service.ArticleService;
+
+import at.c02.bpj.client.service.CategoryService;
 import de.saxsys.mvvmfx.ViewModel;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
@@ -21,16 +35,28 @@ public class ArticleViewModel implements ViewModel {
 	/**
 	 * Liste aller Artikel
 	 */
+	
 	private ObservableList<Article> articles = FXCollections.observableArrayList();
-
+	private ObservableList<Category> categoryList = FXCollections.observableArrayList();
 	private ArticleService articleService;
 
-	// ArticleService wird mittels Construktor-Injection gesetzt
-	public ArticleViewModel(ArticleService articleService) {
-		this.articleService = articleService;
+	private FilteredList<Article> filteredArticleList = new FilteredList<>(articles);
+	
+	// Search Field Properties für Bindings
+	private ObjectProperty<Category> searchCategory = new SimpleObjectProperty<>();
+	private StringProperty searchArticleID = new SimpleStringProperty();
+	private StringProperty searchArticleName = new SimpleStringProperty();
+	
+	// Service Klassen mittels Construktor-Injection setzen
+	public ArticleViewModel(CategoryService categoryService, ArticleService articleService) {
+		Async.executeUILoad(categoryService::getCategories, categoryList::setAll);
+		Async.executeUILoad(articleService::getArticles, articles::setAll);
+		//this.articleService = articleService;
 
-		loadArticles();
+		//loadArticles();
 	}
+	
+
 
 	/**
 	 * Lädt die Artikel
@@ -40,9 +66,13 @@ public class ArticleViewModel implements ViewModel {
 	}
 
 	public ObservableList<Article> articlesProperty() {
-		return articles;
+		return filteredArticleList;
 	}
 
+	public ObservableList<Category> categoryProperty() {
+		return categoryList;
+	}
+	
 	public List<Article> getArticles() {
 		return articles;
 	}
@@ -86,6 +116,7 @@ public class ArticleViewModel implements ViewModel {
 	 * 
 	 * @param article
 	 */
+
 	public void deleteArticle(Article article) {
 		// Sicherheitsabfrage
 		Alert alert = new Alert(AlertType.WARNING);
@@ -99,7 +130,89 @@ public class ArticleViewModel implements ViewModel {
 			loadArticles();
 		}
 	}
+		public void onSearchButtonClick() {
+		
+		if (Strings.isNullOrEmpty(searchArticleID.getValue()) &&  searchArticleName.getValue() == null &&
+				searchArticleID.getValue() == null) {
+		
+				 Alert noInputAlert= new Alert(AlertType.WARNING);
+			        noInputAlert.setHeaderText("Keine Eingabe vorhanden");
+			        noInputAlert.setContentText("Bitte mindestens ein Suchkriterium eingeben");
+			        noInputAlert.showAndWait();
+		}
+		
+		filteredArticleList.setPredicate(this::checkArticleMatchesSearch);
+		
+		if (filteredArticleList.isEmpty()) {
+			Alert noMatchFoundAlert= new Alert(AlertType.INFORMATION);
+	        noMatchFoundAlert.setHeaderText("Kein Angebot mit angegebenen Suchkriterien gefunden");
+			noMatchFoundAlert.setContentText("Bitte prüfen Sie die angegebenen Suchkriterien");
+	        noMatchFoundAlert.showAndWait();
+		}
+	}
+		
+		private boolean checkArticleMatchesSearch(Article article) {
+			String articleId = searchArticleID.getValue();
+			Category category = searchCategory.getValue();
+			String articleName = searchArticleName.getValue();
 
+			if (!Strings.isNullOrEmpty(articleId) && !Objects.equal(article.getArticleId(), Longs.tryParse(articleId))) {
+				return false;
+			}
+			
+			if (!Strings.isNullOrEmpty(articleName) && !Objects.equal(article.getName(), articleName)) {
+				return false;
+			}
+			
+
+
+			if (category != null && !Objects.equal(category.getCategoryId(), article.getCategory().getCategoryId())) {
+				return false;
+			}
+
+			return true;
+		}
+		
+		public ObservableList<Category> categoryListProperty() {
+			return categoryList;
+		}
+		public ObservableList<Article> articleListProperty() {
+			return filteredArticleList;
+		}
+		public final StringProperty searchArticleIdProperty() {
+			return this.searchArticleID;
+		}
+		public final java.lang.String getSearchArticleId() {
+			return this.searchArticleIdProperty().get();
+		}
+		public final StringProperty searchArticleNameProperty() {
+			return this.searchArticleName;
+		}
+		public final java.lang.String getSearchArticleName() {
+			return this.searchArticleNameProperty().get();
+		}
+		public final void setSearchArticleId(final java.lang.String searchArticleId) {
+			this.searchArticleIdProperty().set(searchArticleId);
+		}
+		public final ObjectProperty<Category> searchCategoryProperty() {
+			return this.searchCategory;
+		}
+
+		public final at.c02.bpj.client.api.model.Category getSearchCategory() {
+			return this.searchCategoryProperty().get();
+		}
+
+		public final void setSearchCategory(final at.c02.bpj.client.api.model.Category searchCategory) {
+			this.searchCategoryProperty().set(searchCategory);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
 	// UC006 öffnen des OfferManagements
 	public void openOfferManagement() {
 		OfferManagementDialog dialog = new OfferManagementDialog();
