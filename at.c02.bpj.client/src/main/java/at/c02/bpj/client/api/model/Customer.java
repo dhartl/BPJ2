907bd0650de290a9ca5d3217017269b949fb1555
Philipp <philippg@outlook.at>
2016-06-25 19:31:42 +0200

    Properties mit gettern und settern für City, street, housenr, postcode

--- at.c02.bpj.client/src/main/java/at/c02/bpj/client/api/model/Customer.java
+++ at.c02.bpj.client/src/main/java/at/c02/bpj/client/api/model/Customer.java
@@ -7,76 +7,127 @@ import javafx.beans.property.StringProperty;
 
 public class Customer {
 
+    private ObjectProperty<Long> customerID = new SimpleObjectProperty<>();
+    private StringProperty companyName = new SimpleStringProperty();
+    private StringProperty contactFirstName = new SimpleStringProperty();
+    private StringProperty contactLastName = new SimpleStringProperty();
+    private StringProperty city = new SimpleStringProperty();
+    private StringProperty street = new SimpleStringProperty();
+    private StringProperty houseNr = new SimpleStringProperty();
+    private StringProperty postCode = new SimpleStringProperty();
 
-	private ObjectProperty<Long> customerID = new SimpleObjectProperty<>();
-	private StringProperty companyName = new SimpleStringProperty();
-	private StringProperty contactFirstName = new SimpleStringProperty();
-	private StringProperty contactLastName = new SimpleStringProperty();
-	
+    // kein Argument-Konstruktor UNBEDINGT erforderlich
+    public Customer() {
+    }
 
-	// kein Argument-Konstruktor UNBEDINGT erforderlich
-	public Customer() {
-	}
+    public final ObjectProperty<Long> customerIdProperty() {
+	return this.customerID;
+    }
 
-	
-	public final ObjectProperty<Long> customerIdProperty() {
-		return this.customerID;
-	}
-	
-	public Long getCustomerID() {
-		return this.customerIdProperty().get();
-	}
+    public Long getCustomerID() {
+	return this.customerIdProperty().get();
+    }
 
-	public void setCustomerID(final Long customerID) {
-		this.customerIdProperty().set(customerID);
-	}
+    public void setCustomerID(final Long customerID) {
+	this.customerIdProperty().set(customerID);
+    }
 
+    public final StringProperty companyNameProperty() {
+	return this.companyName;
+    }
 
-	
-	public final StringProperty companyNameProperty() {
-		return this.companyName;
-	}
-	
-	public final java.lang.String getCompanyName() {
-		return this.companyNameProperty().get();
-	}
+    public final java.lang.String getCompanyName() {
+	return this.companyNameProperty().get();
+    }
 
-	public final void setCompanyName(final java.lang.String companyName) {
-		this.companyNameProperty().set(companyName);
-	}
+    public final void setCompanyName(final java.lang.String companyName) {
+	this.companyNameProperty().set(companyName);
+    }
 
+    public final StringProperty contactFirstNameProperty() {
+	return this.contactFirstName;
+    }
 
-	
-	
-	public final StringProperty contactFirstNameProperty() {
-		return this.contactFirstName;
-	}
-	
-	public final java.lang.String getContactFirstName() {
-		return this.contactFirstNameProperty().get();
-	}
+    public final java.lang.String getContactFirstName() {
+	return this.contactFirstNameProperty().get();
+    }
 
-	public final void setContactFirstName(final java.lang.String contactFirstName) {
-		this.contactFirstNameProperty().set(contactFirstName);
-	}
+    public final void setContactFirstName(final java.lang.String contactFirstName) {
+	this.contactFirstNameProperty().set(contactFirstName);
+    }
 
-	
-	public final StringProperty contactLasttNameProperty() {
-		return this.contactLastName;
-	}
-	
-	public final java.lang.String getContactLastName() {
-		return this.contactFirstNameProperty().get();
-	}
+    public final StringProperty contactLasttNameProperty() {
+	return this.contactLastName;
+    }
 
-	public final void setContactLastName(final java.lang.String contactLastName) {
-		this.contactFirstNameProperty().set(contactLastName);
-	}
+    public final java.lang.String getContactLastName() {
+	return this.contactFirstNameProperty().get();
+    }
 
-	@Override
-	public String toString() {
-		return "Customer [customerID=" + customerID + ", companyName=" + companyName + ", contactFirstName="
-				+ contactFirstName + ", contactLastName=" + contactLastName + "]";
-	}
-	
+    public final void setContactLastName(final java.lang.String contactLastName) {
+	this.contactFirstNameProperty().set(contactLastName);
+    }
+
+    // Property für Straße
+
+    public final StringProperty streetProperty() {
+	return this.street;
+    }
+
+    public final java.lang.String getStreetProperty() {
+	return this.streetProperty().get();
+    }
+
+    public final void setStreetProperty(final java.lang.String street) {
+	this.streetProperty().set(street);
+    }
+
+    // Property für Stadt
+
+    public final StringProperty cityProperty() {
+	return this.city;
+    }
+
+    public final java.lang.String getCityProperty() {
+	return this.cityProperty().get();
+    }
+
+    public final void setCityProperty(final java.lang.String city) {
+	this.cityProperty().set(city);
+    }
+
+    // Property für Hausnummer
+
+    public final StringProperty houseNrProperty() {
+	return this.houseNr;
+    }
+
+    public final java.lang.String getHouseNrProperty() {
+	return this.houseNrProperty().get();
+    }
+
+    public final void setHouseNrProperty(final java.lang.String houseNr) {
+	this.houseNrProperty().set(houseNr);
+    }
+
+    // Property für PLZ
+
+    public final StringProperty postCodeProperty() {
+	return this.postCode;
+    }
+
+    public final java.lang.String getPostCodeProperty() {
+	return this.postCodeProperty().get();
+    }
+
+    public final void setPostCodeProperty(final java.lang.String postCode) {
+	this.postCodeProperty().set(postCode);
+    }
+
+    @Override
+    public String toString() {
+	return "Customer [customerID=" + customerID + ", companyName=" + companyName + ", contactFirstName="
+		+ contactFirstName + ", contactLastName=" + contactLastName + "]";
+    }
+
 }
