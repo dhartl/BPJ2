6a56cb73b14c923424e97486a7226ed7a123d87a
Daniel Hartl <daniel_hartl@gmx.at>
2016-07-04 22:03:04 +0200

    StatusLine mit aktuellem Benutzernamen

--- at.c02.bpj.client/src/main/java/at/c02/bpj/client/AuthContext.java
+++ at.c02.bpj.client/src/main/java/at/c02/bpj/client/AuthContext.java
@@ -1,10 +1,12 @@
 package at.c02.bpj.client;
 
 import at.c02.bpj.client.api.model.Employee;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 
 public class AuthContext {
 	private static AuthContext INSTANCE;
-	private Employee currentUser;
+	private ObjectProperty<Employee> currentUser = new SimpleObjectProperty<>();
 
 	public static AuthContext getInstance() {
 		if (INSTANCE == null) {
@@ -13,11 +15,16 @@ public class AuthContext {
 		return INSTANCE;
 	}
 
-	public void setCurrentUser(Employee currentUser) {
-		this.currentUser = currentUser;
+	public final ObjectProperty<Employee> currentUserProperty() {
+		return this.currentUser;
 	}
 
-	public Employee getCurrentUser() {
-		return currentUser;
+	public final at.c02.bpj.client.api.model.Employee getCurrentUser() {
+		return this.currentUserProperty().get();
 	}
+
+	public final void setCurrentUser(final at.c02.bpj.client.api.model.Employee currentUser) {
+		this.currentUserProperty().set(currentUser);
+	}
+
 }
--- at.c02.bpj.client/src/main/java/at/c02/bpj/client/ui/MainViewModel.java
+++ at.c02.bpj.client/src/main/java/at/c02/bpj/client/ui/MainViewModel.java
@@ -1,11 +1,16 @@
 package at.c02.bpj.client.ui;
 
+import at.c02.bpj.client.AuthContext;
+import at.c02.bpj.client.api.model.Employee;
 import de.saxsys.mvvmfx.Context;
 import de.saxsys.mvvmfx.FluentViewLoader;
 import de.saxsys.mvvmfx.ViewModel;
 import javafx.beans.binding.Bindings;
+import javafx.beans.binding.StringBinding;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
@@ -14,11 +19,30 @@ public class MainViewModel implements ViewModel {
 	private ObjectProperty<Node> centerNode = new SimpleObjectProperty<>();
 	private ObservableList<MainMenuItem> mainMenuItems = FXCollections.observableArrayList();
 	private ObjectProperty<MainMenuItem> selectedMenuItem = new SimpleObjectProperty<>();
+	private StringProperty username = new SimpleStringProperty();
 
 	private Context context;
 
 	public MainViewModel() {
 		centerNode.bind(Bindings.createObjectBinding(this::updateCenterNode, selectedMenuItem));
+		StringBinding usernameBinding = Bindings.selectString(AuthContext.getInstance().currentUserProperty(),
+				"username");
+		StringBinding firstNameBinding = Bindings.selectString(AuthContext.getInstance().currentUserProperty(),
+				"firstname");
+		StringBinding lastNameBinding = Bindings.selectString(AuthContext.getInstance().currentUserProperty(),
+				"lastname");
+
+		username.bind(
+				Bindings.createStringBinding(this::getUserInfo, usernameBinding, firstNameBinding, lastNameBinding));
+	}
+
+	private String getUserInfo() {
+		Employee currentUser = AuthContext.getInstance().getCurrentUser();
+		if (currentUser == null) {
+			return "";
+		}
+		return String.format("Angemeldet als: %s (%s %s)", currentUser.getUsername(), currentUser.getFirstname(),
+				currentUser.getLastname());
 	}
 
 	private Node updateCenterNode() {
@@ -60,6 +84,18 @@ public class MainViewModel implements ViewModel {
 
 	public void setContext(Context context) {
 		this.context = context;
+	}
+
+	public final StringProperty usernameProperty() {
+		return this.username;
+	}
+
+	public final java.lang.String getUsername() {
+		return this.usernameProperty().get();
+	}
+
+	public final void setUsername(final java.lang.String username) {
+		this.usernameProperty().set(username);
 	}
 
 }
