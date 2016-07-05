409fd165585023eb48d9fab9cfd6930f507df0fe
Daniel Hartl <daniel_hartl@gmx.at>
2016-07-06 00:27:42 +0200

    Test Beispiel

--- at.c02.bpj.client/src/test/java/at/c02/bpj/client/offer/OfferCreateViewTest.java
+++ at.c02.bpj.client/src/test/java/at/c02/bpj/client/offer/OfferCreateViewTest.java
@@ -1,4 +1,8 @@
+package at.c02.bpj.client.offer;
 
+import java.util.List;
+
+import org.assertj.core.util.Lists;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,143 +15,153 @@ import at.c02.bpj.client.api.model.Customer;
 import at.c02.bpj.client.api.model.Employee;
 import at.c02.bpj.client.api.model.Offer;
 import at.c02.bpj.client.api.model.OfferPosition;
-import at.c02.bpj.client.service.CustomerService;
-import at.c02.bpj.client.service.EmployeeService;
+import at.c02.bpj.client.service.ArticleService;
 import at.c02.bpj.client.service.OfferService;
 import at.c02.bpj.client.test.MvvmFxGuiTest;
+import at.c02.bpj.client.test.TestData;
 import de.saxsys.mvvmfx.FxmlView;
+import de.saxsys.mvvmfx.Scope;
 import de.saxsys.mvvmfx.ViewModel;
 import eu.lestard.easydi.EasyDI;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 
 @RunWith(MockitoJUnitRunner.class)
 public class OfferCreateViewTest extends MvvmFxGuiTest {
 
-    @Mock
-    private OfferService offerService;
+	@Mock
+	private OfferService offerService;
 
-    @Mock
-    private EmployeeService employeeService;
+	@Mock
+	private ArticleService articleService;
 
-    @Mock
-    private CustomerService customerService;
+	private Offer offer1;
+	private Offer offer2;
+	private Employee employee1;
+	private Customer customer1;
 
-    private Offer offer1;
-    private Offer offer2;
-    private Employee employee1;
-    private Customer customer1;
+	@Override
+	public Class<? extends FxmlView<? extends ViewModel>> getViewClass() {
+		return OfferCreateView.class;
+	}
 
-    @Override
-    public Class<? extends FxmlView<? extends ViewModel>> getViewClass() {
-	return OfferCreateView.class;
-    }
+	@Override
+	public List<Scope> getViewScopes() {
+		return Lists.newArrayList(new OfferScope());
+	}
 
-    @Override
-    public void setupContext(EasyDI context) {
+	@Override
+	public void setupContext(EasyDI context) {
+		context.bindInstance(OfferService.class, offerService);
+		context.bindInstance(ArticleService.class, articleService);
 
-	Mockito.when(offerService.saveOffer(offer1)).thenReturn(offer1);
-	context.bindInstance(OfferService.class, offerService);
-	   }
+		Mockito.when(articleService.getArticles()).thenReturn(Lists.newArrayList(TestData.article1()));
 
-    @Test
-    public void testInitialize() {
-	TableView<OfferPosition> tblOfferPositions = find("#tblOfferPositions");
+		Mockito.when(offerService.saveOffer(offer1)).thenReturn(offer1);
+		stage.setWidth(1000);
+		stage.setHeight(1000);
+	}
 
-	TableView<Article> tblArticles = find("#tblArticles");
+	@Test
+	public void testInitialize() {
+		TableView<OfferPosition> tblOfferPositions = find("#tblOfferPositions");
 
-    }
+		TableView<Article> tblArticles = find("#tblArticles");
 
-    // #007 Der User wählt in der Artikelansicht einen Artikel aus und fügt ihn
-    // zum Angebot hinzu.
-    @Test
-    public void testAddArticleToOffer() {
-	TableView<OfferPosition> tblOfferPositions = find("#tblOfferPositions");
+	}
 
-	TableView<Article> tblArticles = find("#tblArticles");
-	click("#tblArticles").click(MouseButton.SECONDARY).click(MouseButton.PRIMARY);
+	// #007 Der User wählt in der Artikelansicht einen Artikel aus und fügt ihn
+	// zum Angebot hinzu.
+	@Test
+	public void testAddArticleToOffer() {
+		TableView<OfferPosition> tblOfferPositions = find("#tblOfferPositions");
 
-	Assert.assertEquals(1, tblOfferPositions.getItems().size());
-    }
+		TableView<Article> tblArticles = find("#tblArticles");
+		click("Artikel1", MouseButton.SECONDARY).click("Zum Angebot hinzufügen");
 
-    // #008 Der User möchte das Angebot abschließen und klickt auf Speichern.
-    // Es wurde jedoch einer Position die Menge 100 zugeordnet .
-    @Test
-    public void testSaveOfferAmountError() {
-	TableView<OfferPosition> tblOfferPositions = find("#tblOfferPositions");
+		Assert.assertEquals(1, tblOfferPositions.getItems().size());
+	}
 
-	TableView<Article> tblArticles = find("#tblArticles");
-	click("#tblArticles").click(MouseButton.SECONDARY).click(MouseButton.PRIMARY);
-	// Eingabe von 100 bei Amount?
+	// #008 Der User möchte das Angebot abschließen und klickt auf Speichern.
+	// Es wurde jedoch einer Position die Menge 100 zugeordnet .
+	@Test
+	public void testSaveOfferAmountError() {
+		TableView<OfferPosition> tblOfferPositions = find("#tblOfferPositions");
 
-	// Hier Button drücken
-	Button btnSaveAndClose = find("#btnSaveAndClose");
-	click("#btnSaveAndClose");
-	// Warnung muss aufscheinen
+		TableView<Article> tblArticles = find("#tblArticles");
+		click("Artikel1", MouseButton.SECONDARY).click("Zum Angebot hinzufügen");
+		// Eingabe von 100 bei Amount?
+		move(find("Menge")).moveBy(-10, 25).doubleClick(MouseButton.PRIMARY).doubleClick().type("100")
+				.press(KeyCode.ENTER);
+		// Hier Button drücken
+		OfferCreateViewModel viewModel = (OfferCreateViewModel) getViewTuple().getViewModel();
+		viewModel.saveOffer();
+		// Warnung muss aufscheinen
 
-    }
+	}
 
-    // #009 Der User möchte das Angebot abschließen und klickt auf Speichern.
-    // Es wurde jedoch einem Artikel ein negativer Preis (Preis € -100)
-    // zugeordnet.
-    @Test
-    public void testSaveOfferPriceError() {
-	TableView<OfferPosition> tblOfferPositions = find("#tblOfferPositions");
+	// #009 Der User möchte das Angebot abschließen und klickt auf Speichern.
+	// Es wurde jedoch einem Artikel ein negativer Preis (Preis € -100)
+	// zugeordnet.
+	@Test
+	public void testSaveOfferPriceError() {
+		TableView<OfferPosition> tblOfferPositions = find("#tblOfferPositions");
 
-	TableView<Article> tblArticles = find("#tblArticles");
-	click("#tblArticles").click(MouseButton.SECONDARY).click(MouseButton.PRIMARY);
-	// Eingabe von -100 bei Price?
+		TableView<Article> tblArticles = find("#tblArticles");
+		click("#tblArticles").click(MouseButton.SECONDARY).click(MouseButton.PRIMARY);
+		// Eingabe von -100 bei Price?
 
-	// Hier Button drücken
-	Button btnSaveAndClose = find("#btnSaveAndClose");
-	click("#btnSaveAndClose");
-	// Warnung muss aufscheinen
+		// Hier Button drücken
+		Button btnSaveAndClose = find("#btnSaveAndClose");
+		click("#btnSaveAndClose");
+		// Warnung muss aufscheinen
 
-    }
+	}
 
-    // #010 Der User möchte das Angebot abschließen und klickt auf Speichern. Es
-    // wurde jedoch bisher kein Artikel (keine Position) dem Angebot
-    // hinzugefügt.
+	// #010 Der User möchte das Angebot abschließen und klickt auf Speichern. Es
+	// wurde jedoch bisher kein Artikel (keine Position) dem Angebot
+	// hinzugefügt.
 
-    @Test
-    public void testSaveOfferOfferPositionError() {
-	TableView<OfferPosition> tblOfferPositions = find("#tblOfferPositions");
+	@Test
+	public void testSaveOfferOfferPositionError() {
+		TableView<OfferPosition> tblOfferPositions = find("#tblOfferPositions");
 
-	TableView<Article> tblArticles = find("#tblArticles");
+		TableView<Article> tblArticles = find("#tblArticles");
 
-	// Hier Button drücken
-	Button btnSaveAndClose = find("#btnSaveAndClose");
-	click("#btnSaveAndClose");
-	// Warnung muss aufscheinen
+		// Hier Button drücken
+		Button btnSaveAndClose = find("#btnSaveAndClose");
+		click("#btnSaveAndClose");
+		// Warnung muss aufscheinen
 
-    }
+	}
 
-    // #011 Der User tätigt folgende Eingabe: Artikel= Prüfstand xy; Preis= €
-    // 10.000,--, Menge= 1 und klickt folgend auf „Angebot speichern“.
-    @Test
-    public void testSaveOfferCompleted() {
-	TableView<OfferPosition> tblOfferPositions = find("#tblOfferPositions");
+	// #011 Der User tätigt folgende Eingabe: Artikel= Prüfstand xy; Preis= €
+	// 10.000,--, Menge= 1 und klickt folgend auf „Angebot speichern“.
+	@Test
+	public void testSaveOfferCompleted() {
+		TableView<OfferPosition> tblOfferPositions = find("#tblOfferPositions");
 
-	TableView<Article> tblArticles = find("#tblArticles");
+		TableView<Article> tblArticles = find("#tblArticles");
 
-	// Hier Button drücken
-	Button btnSaveAndClose = find("#btnSaveAndClose");
-	click("#btnSaveAndClose");
+		// Hier Button drücken
+		Button btnSaveAndClose = find("#btnSaveAndClose");
+		click("#btnSaveAndClose");
 
-	// Assert.assertEquals(getViewTuple().getViewModel().???,
-	// btnSaveAndClose.getOnAction());
-    }
+		// Assert.assertEquals(getViewTuple().getViewModel().???,
+		// btnSaveAndClose.getOnAction());
+	}
 
-    // #012 Der User schließt die Angebotserstellungsmaske und möchte somit die
-    // aktuelle Erstellung des Angebots verwerfen. @Test
-    public void testCloseOfferCreateView() {
-	TableView<OfferPosition> tblOfferPositions = find("#tblOfferPositions");
+	// #012 Der User schließt die Angebotserstellungsmaske und möchte somit die
+	// aktuelle Erstellung des Angebots verwerfen. @Test
+	public void testCloseOfferCreateView() {
+		TableView<OfferPosition> tblOfferPositions = find("#tblOfferPositions");
 
-	TableView<Article> tblArticles = find("#tblArticles");
+		TableView<Article> tblArticles = find("#tblArticles");
 
-	this.closeCurrentWindow();
+		this.closeCurrentWindow();
 
-    }
+	}
 
 }
--- at.c02.bpj.client/src/test/java/at/c02/bpj/client/test/MvvmFxGuiTest.java
+++ at.c02.bpj.client/src/test/java/at/c02/bpj/client/test/MvvmFxGuiTest.java
@@ -1,11 +1,15 @@
 package at.c02.bpj.client.test;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.loadui.testfx.GuiTest;
 
 import at.c02.bpj.client.Async;
 import de.saxsys.mvvmfx.FluentViewLoader;
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.MvvmFX;
+import de.saxsys.mvvmfx.Scope;
 import de.saxsys.mvvmfx.ViewModel;
 import de.saxsys.mvvmfx.ViewTuple;
 import eu.lestard.easydi.EasyDI;
@@ -25,12 +29,16 @@ public abstract class MvvmFxGuiTest extends GuiTest {
 		EasyDI context = new EasyDI();
 		setupContext(context);
 		MvvmFX.setCustomDependencyInjector(context::getInstance);
-		viewTuple = FluentViewLoader.fxmlView(getViewClass()).load();
+		viewTuple = FluentViewLoader.fxmlView(getViewClass()).providedScopes(getViewScopes()).load();
 		return viewTuple.getView();
 	}
 
 	public abstract Class<? extends FxmlView<? extends ViewModel>> getViewClass();
 
 	public abstract void setupContext(EasyDI context);
+
+	public List<Scope> getViewScopes() {
+		return Collections.emptyList();
+	}
 
 }
